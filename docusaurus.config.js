5a0fa4a34354bd51f704c35a32aec28e60ba097d
SantiagoCabana <santiagoadancabana@hotmail.com>
2024-03-18 09:36:37 -0500

    config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/B01_Hotel.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'SantiagoCabana', // Usually your GitHub org/user name.
+  projectName: 'B01_Hotel.github.io', // Usually your repo name.
+  deploymentBranch:'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
