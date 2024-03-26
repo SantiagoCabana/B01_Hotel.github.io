968032b01d4ddfbeac55c4c7b34cf0382f65697e
FutureTripod <santiagoadancabana@hotmail.com>
2024-03-26 00:20:58 -0500

    cambios notables

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Documentacion Hotel`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
